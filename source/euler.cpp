2b6e38a48fe986358c02ecaa16859c858c1a54ae
Eleftherios Avramidis <el.avramidis@gmail.com>
2019-12-10 17:07:37 +0000

    Added extra example with sin

--- source/euler.cpp
+++ source/euler.cpp
@@ -1,6 +1,9 @@
 #include<CL/sycl.hpp>
 #include<iostream>
+
 constexpr int num = 16;
+
+#define SIZE 1024
 
 using namespace cl::sycl;
 
@@ -11,15 +14,17 @@ int main()
 	std::cout << "Selected device: " << myQueue.get_device().get_info<info::device::name>() << std::endl;
 
 	auto R = range<1>{num};
-	buffer<int> A{R};
+	buffer<double> A{R};
 
-	queue{}.submit([&](handler& h) {
+	auto q = queue{}.submit([&](handler& h) {
 	  auto out =
 			  A.get_access<access::mode::write>(h);
 	  h.parallel_for<class ex1>(R, [=](id<1> idx) {
-		out[idx] = idx[0];
+		out[idx] = cl::sycl::sin((double)idx[0]);
 	  });
 	});
+	q.wait();
+
 
 	auto result = A.get_access<access::mode::read>();
 
